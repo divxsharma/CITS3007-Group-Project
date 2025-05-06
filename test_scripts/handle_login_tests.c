0e92edbb1d16603e9555f6e5ec452aa4635e649d
praj107 <pranavrajput321@gmail.com>
2025-05-06 20:39:43 +0800

    Added third test (ban check) Refs #6

--- test_scripts/handle_login_tests.c
+++ test_scripts/handle_login_tests.c
@@ -73,7 +73,7 @@ void log_message(log_level_t level, const char *fmt, ...) {
 
 /* Mock lookup function to simulate the database.
  * Each case will have its own unique mock account lookup return.
- * Run with: gcc test_scripts/handle_login_tests.c src/login.c src/account.c -I./src -lcheck -lsodium -lsubunit -lpthread -lrt -lm -o test_scripts/handle_login_tests
+ * Run with: gcc test_scripts/handle_login_tests.c src/logging.h src/login.c src/account.c -I./src -lcheck -lsodium -lsubunit -lpthread -lrt -lm -o test_scripts/handle_login_tests
  */
 bool account_lookup_by_userid(const char *userid, account_t *result) {
     if (strcmp(userid, "test1") == 0) {
@@ -83,6 +83,16 @@ bool account_lookup_by_userid(const char *userid, account_t *result) {
         result->expiration_time = time(NULL) + 24*60*60;
         // Use the real password hashing function
         account_update_password(result, "hashpass1");
+        return true;
+    }
+
+    if (strcmp(userid, "test3") == 0) {
+        result->account_id = 2;
+        strcpy(result->userid, "test3");
+        result->unban_time = time(NULL) + 1e7; // Set a future unban time
+        result->expiration_time = time(NULL) + 24*60*60;
+        // Use the real password hashing function
+        account_update_password(result, "hashpass3");
         return true;
     }
     return false;
@@ -97,12 +107,12 @@ START_TEST(test_handle_login_success) {
     int log_fd = STDERR_FILENO; // Use standard error for logging
     time_t login_time = time(NULL);
     ip4_addr_t client_ip = {127001}; // Localhost IP
-    log_message(LOG_INFO, "Testing successful login for user: test1");
-    login_result_t result = handle_login("test2", "hashpass1", client_ip, login_time, client_output_fd, log_fd, &session);
-    log_message(LOG_INFO, "Login result: %d", result);
+    log_message(LOG_INFO, "[TEST] Testing successful login for user: test1");
+    login_result_t result = handle_login("test1", "hashpass1", client_ip, login_time, client_output_fd, log_fd, &session);
+    log_message(LOG_INFO, "[TEST] Login result: %d", result);
     ck_assert_int_eq(result, LOGIN_SUCCESS);
     ck_assert_int_eq(session.account_id, 1);
-    ck_assert_int_eq(session.session_start, login_time);
+    ck_assert_int_eq(session.session_start, login_time); 
     ck_assert_int_eq(session.expiration_time, login_time + 24 * 60 * 60);
 } END_TEST
 
@@ -116,13 +126,32 @@ START_TEST(test_handle_login_user_not_found) {
   int log_fd = STDERR_FILENO; // Use standard error for logging
   time_t login_time = time(NULL);
   ip4_addr_t client_ip = {127001}; // Localhost IP
-  log_message(LOG_INFO, "Testing Unsuccessful user not found login for user: test1");
-  login_result_t result = handle_login("test1", "hashpass1", client_ip, login_time, client_output_fd, log_fd, &session);
-  log_message(LOG_INFO, "Login result: %d", result);
+  log_message(LOG_INFO, "[TEST] Testing Unsuccessful user not found login for user: test1");
+  login_result_t result = handle_login("test2", "hashpass1", client_ip, login_time, client_output_fd, log_fd, &session);
+  log_message(LOG_INFO, "[TEST] Login result: %d", result);
   ck_assert_int_eq(result, LOGIN_FAIL_USER_NOT_FOUND);
   ck_assert_int_eq(session.account_id, 1);
-  ck_assert_int_eq(session.session_start, login_time);
-  ck_assert_int_eq(session.expiration_time, login_time + 24 * 60 * 60);
+  // ck_assert_int_eq(session.session_start, login_time); // Removed because early return is expected.
+  // ck_assert_int_eq(session.expiration_time, login_time + 24 * 60 * 60);  // ^
+} END_TEST
+
+/* 
+ * Third test case: Expected User is banned
+ * This test case simulates a login attempt with a currently banned user.
+ */
+START_TEST(test_handle_login_user_banned) {
+  login_session_data_t session;
+  int client_output_fd = STDOUT_FILENO; // Use standard output for testing
+  int log_fd = STDERR_FILENO; // Use standard error for logging
+  time_t login_time = time(NULL);
+  ip4_addr_t client_ip = {127001}; // Localhost IP
+  log_message(LOG_INFO, "[TEST] Testing banned user login: test1");
+  login_result_t result = handle_login("test3", "hashpass1", client_ip, login_time, client_output_fd, log_fd, &session);
+  log_message(LOG_INFO, "[TEST] Login result: %d", result);
+  ck_assert_int_eq(result, LOGIN_FAIL_ACCOUNT_BANNED);
+  ck_assert_int_eq(session.account_id, 1);
+  // ck_assert_int_eq(session.session_start, login_time); // Removed because early return is expected.
+  // ck_assert_int_eq(session.expiration_time, login_time + 24 * 60 * 60);  // ^
 } END_TEST
 
 int main(void) {
@@ -131,7 +160,8 @@ int main(void) {
 
     // Add test cases to the test case
     tcase_add_test(tcase, test_handle_login_success);
-
+    tcase_add_test(tcase, test_handle_login_user_not_found);
+    tcase_add_test(tcase, test_handle_login_user_banned);
     // Add the test case to the suite
     suite_add_tcase(suite, tcase);
 
